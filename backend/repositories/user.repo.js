3c6c82ad5938c686a5ffa739566f82926383b8a2
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2026-04-02 15:45:05 +0200

    feat: add user repository with findAll method

---
+++ backend/repositories/user.repo.js
@@ -0,0 +1,10 @@
+class UserRepository {
+  constructor(pool) {
+    this.pool = pool;
+  }
+
+  async findAll() {
+    const { rows } = await this.pool.query("SELECT * FROM users;");
+    return rows;
+  }
+}
