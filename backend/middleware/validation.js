d548c7492d9ba37faace65725ddf8d8125834a42
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2026-04-04 17:49:49 +0200

    fix: fix spelling

--- backend/middleware/validation.js
+++ backend/middleware/validation.js
@@ -5,7 +5,7 @@ const userSchemas = {
     username: Joi.string().alphanum().min(3).max(50).required(),
     email: Joi.string().email().required(),
     password_hash: Joi.string().min(6).required(),
-    age: Joi.number().ingeger().min(13).max(150),
+    age: Joi.number().integer().min(13).max(150),
     isActive: Joi.boolean(),
   }),
   id: Joi.object({
