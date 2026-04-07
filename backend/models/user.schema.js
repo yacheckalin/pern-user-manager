189b1b37a508ace971990f5254c5c3e9211c27ee
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2026-04-07 15:07:00 +0200

    feat: add Joi object for updateUser schema

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -11,6 +11,21 @@ const userSchemas = {
   id: Joi.object({
     id: Joi.number().integer().positive().required(),
   }),
+  updateUser: Joi.object({
+    username: Joi.string().alphanum().min(3).max(50),
+    email: Joi.string().email(),
+    password: Joi.string().min(6).messages({
+      "string.min": "Password must be at least 6 characters long",
+    }),
+    confirm_password: Joi.any()
+      .equal(Joi.ref("password"))
+      .messages({
+        "any.only": "Password do not match",
+      })
+      .with("password", "confirm_password"),
+    age: Joi.number().integer().min(13).max(150),
+    is_active: Joi.boolean(),
+  }),
 };
 
 export { userSchemas };
