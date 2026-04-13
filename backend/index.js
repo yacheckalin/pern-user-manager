d27b1c5716b358cbef3b056619ff4cc3a91d13ff
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2026-04-13 11:56:51 +0200

    refactor: add gracefull shutdown on SIGTERM

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/user-routes.js";
 import errorHandler from "./middleware/error-handler.js";
+import db from './config/database.js';
 
 const { PORT } = process.env;
 
@@ -14,6 +15,13 @@ app.use(express.json());
 app.use("/users", userRoutes);
 
 app.use(errorHandler);
+
+// Graceful shutdown
+process.on('SIGTERM', async () => {
+  console.log('SIGTERM received, closing HTTP server...');
+  await db.end();
+  process.exit(0);
+});
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () =>
