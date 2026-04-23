07029cabba6eaecc77814f3099dc6c9b9dac01f5
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2026-04-23 07:54:46 +0200

    chore: install prom-client

---
+++ backend/metrics.js
@@ -0,0 +1,9 @@
+import client from "prom-client";
+
+const register = client.register;
+
+export const httpRequestDuration = new client.Histogram({
+  name: "http_request_duration_seconds",
+  help: "Duration of HTTP requests in seconds",
+  labelNames: ["method", "route", "status_code"],
+});
---
+++ backend/middleware/metrics.js
@@ -0,0 +1,15 @@
+import { httpRequestDuration } from "../metrics.js"
+
+export function metrics () {
+  return (req, res, next) {
+    const end = httpRequestDuration.startTimer();
+
+    res.on("finish", () => {
+      end({method: req.method, route: req.route ? req.route.path : req.path,
+        status_code: res.statusCode
+      })
+    })
+
+    next()
+  }
+}
