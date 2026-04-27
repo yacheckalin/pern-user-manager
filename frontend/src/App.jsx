102ddf4708ecce8f3820d289a4030725e164b0f4
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2026-04-27 09:21:30 +0200

    fix: fixed routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,41 @@ import "./App.css";
 import UsersPage from "./pages/UsersPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AdminLayout from "@layouts/admin";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigate to="/users" replace />,
+    },
+    {
+      element: <AdminLayout />,
+      children: [
+        {
+          path: "users",
+          element: <UsersPage />,
+          handle: { title: "Management Users" },
+        },
+        {
+          path: "settings",
+          element: <div>Settings</div>,
+          handle: { title: "Settings" },
+        },
+      ],
+    },
+  ]);
+
   return (
     <div className="App">
       <ToastContainer position="top-right" autoClose={3000} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/users" replace />} />
-
-          <Route element={<AdminLayout />}>
-            <Route path="/users" element={<UsersPage />} />
-            <Route path="/settings" element={<div>Settings Page</div>} />
-            <Route path="/analytics" element={<div>Analytics Page</div>} />
-          </Route>
-
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- frontend/src/layouts/components/headers/Header.jsx
+++ frontend/src/layouts/components/headers/Header.jsx
@@ -1,10 +1,14 @@
 import "./Header.css";
+import { useMatches } from "react-router-dom";
 
 const Header = () => {
+  const matches = useMatches();
+  const currentMatch = [...matches].reverse().find((m) => m.handle?.title);
+  const title = currentMatch?.handle?.title || "Admin Panel";
   return (
     <div className="header-wrapper">
       <div className="header-left">
-        <h1 className="page-title">Users Management</h1>
+        <h1 className="page-title">{title}</h1>
       </div>
 
       <div className="header-right">
